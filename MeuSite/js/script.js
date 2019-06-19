261c5c645e3ab1b2405521c3e3e42a761d0de5ed
PauloLuccas <49739211+PauloLuccas@users.noreply.github.com>
2019-06-19 00:32:33 -0300

    Add files via upload

---
+++ MeuSite/js/script.js
@@ -0,0 +1,5 @@
+function nightMode(){
+    var body = document.getElementById('body');
+    var currentClass = body.className;
+    body.className = currentClass == 'light-mode' ? 'dark-mode' : 'light-mode';
+}
